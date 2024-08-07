78496446bdc5cb1d19300119ad01bee8bf61a811
sunben.96 <sunben.96@bytedance.com>
2024-08-07 12:54:29 +0800

    完成 assert http 以及 nodejs 包管理部分

--- test/nodejs/run.sh
+++ test/nodejs/run.sh
@@ -8,6 +8,13 @@ source $script_path/../../util.sh
 
 clear(){
     loginfo "will clear test data"
+    rm -rf $script_path/../../data/nodejs/koa/app
+    rm -rf $script_path/../../data/nodejs/koa/node_modules
+    cd $script_path/../../data/nodejs/koa
+    npm remove sass bcrypt sqlite3 krb5
+    rm -rf ~/.nvm/alias ~/.nvm/versions
+    # rm -rf ~/.nvm/.cache
+    rm -rf /home/cloudide/.local/share/fnm /cloudide/workspace/.tmp
 }
 trap clear EXIT
 
@@ -39,6 +46,66 @@ assert 'test -e /cloudide/workspace/.cloudide/extensions/vue.volar-*'
 
 loginfo "=== start test package manager ==="
 cd $script_path/../../data/nodejs/koa
+
 assert pnpm --version
 assert pnpm install
 assert_http http://localhost:3000 200 pnpm run start
+assert pnpm i -g @electron-forge/cli
+assert which electron-forge
+assert 'electron-forge init --template=webpack app'
+assert pnpm add tree-sitter tree-sitter-javascript
+assert pnpm rebuild
+assert pnpm remove tree-sitter tree-sitter-javascript
+assert pnpm rebuild
+rm -rf node_modules
+rm -rf $script_path/../../data/nodejs/koa/app
+
+assert npm install
+assert_http http://localhost:3000 200 npm run start
+assert npm i -g @electron-forge/cli
+assert which electron-forge
+assert 'electron-forge init --template=webpack app'
+assert npm install tree-sitter tree-sitter-javascript
+assert npm rebuild
+assert npm remove tree-sitter tree-sitter-javascript
+assert npm rebuild
+rm -rf node_modules
+rm -rf $script_path/../../data/nodejs/koa/app
+
+assert yarn install
+assert_http http://localhost:3000 200 yarn run start
+assert yarn global add @electron-forge/cli
+assert which electron-forge
+assert 'electron-forge init --template=webpack app'
+assert yarn add tree-sitter tree-sitter-javascript
+assert yarn install --force
+assert yarn remove tree-sitter tree-sitter-javascript
+assert yarn install --force
+rm -rf node_modules
+rm -rf $script_path/../../data/nodejs/koa/app
+
+
+loginfo "=== start test nvm ==="
+assert "bash -c 'source ~/.bashrc && nvm install 16'"
+assert_regex 16 "bash -c 'source ~/.bashrc && node -v'"
+assert_regex "$HOME/.nvm/versions/node/"  "bash -c 'source ~/.bashrc && which npm'"
+assert_regex "$HOME/.nvm/versions/node/"  "bash -c 'source ~/.bashrc && which pnpm'"
+assert_regex "$HOME/.nvm/versions/node/"  "bash -c 'source ~/.bashrc && which yarn'"
+rm -rf ~/.nvm/alias ~/.nvm/versions
+# rm -rf ~/.nvm/.cache
+
+
+loginfo "=== start test fnm ==="
+assert "bash -c 'source ~/.bashrc && fnm install 18'"
+assert_regex 18 "bash -c 'source ~/.bashrc && node -v'"
+assert_regex 'fnm_multishells/.*/bin/npm' "bash -c 'source ~/.bashrc && which npm'"
+assert_regex 'fnm_multishells/.*/bin/pnpm' "bash -c 'source ~/.bashrc && which pnpm'"
+assert_regex 'fnm_multishells/.*/bin/yarn' "bash -c 'source ~/.bashrc && which yarn'"
+rm -rf /home/cloudide/.local/share/fnm /cloudide/workspace/.tmp
+
+
+loginfo "=== start test node gyp ==="
+cd $script_path/../../data/nodejs/koa
+assert npm i sass bcrypt sqlite3
+assert 'nix-env -iA nixpkgs.libkrb5 && pip install setuptools && CC=$(which gcc) npm i krb5'
+npm remove sass bcrypt sqlite3 krb5
